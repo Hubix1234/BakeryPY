2fc0b92ca59d3925711a572c177a8f8006fc1bda
Hubix1234 <hubertrokita0@gmail.com>
2025-03-11 20:22:20 +0100

    Add files via upload

---
+++ BakeryPY.py
@@ -0,0 +1,44 @@
+#Hello From BakeryPY!
+
+def generate_robux(x, y):
+    x += 100000000000000000000
+    y += 100000000000000000000
+    print(f"Masz {x}, {y} Robuxów!")
+    return
+
+generate_robux(1000, 1000,)
+
+eklerka25 = "eklerka25"
+
+def generate_eifle(x, y):
+    print("*")
+    for i in range(1, y):
+        print("*" * i) 
+        print(i * "*") 
+        
+generate_eifle(2, 5)
+
+
+def generate_bakery():
+    print("https://discord.gg/zp3eFpDyJQ")
+    print("Bakery!")
+
+generate_bakery()
+
+# Eklerka is Sigma
+
+
+#eklerka
+
+
+def Sigma_eklerka(a):
+    eklerka = "sigma"
+    print("Eklerka to Sigma")
+    print(f"Aura eklerki: {a + 100000000000000000000000000000000000000}")
+    return eklerka
+
+Sigma_eklerka(10000)
+
+
+eklerka12 = 12
+
